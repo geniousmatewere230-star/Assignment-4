233c03b9deb0a4ffaecc521da85866029d14b723
Genious Matewere <geniousmatewere230@gmail.com>
2026-05-25 14:34:28 +0200

    Add reference example in C++

--- Reference example.cpp
+++ Reference example.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){    
+    [span_7](start_span)int num = 10;[span_7](end_span)
+    [span_8](start_span)int &ref = num;[span_8](end_span)
+    
+    [span_9](start_span)cout << ref << endl;[span_9](end_span)
+    [span_10](start_span)return 0;[span_10](end_span)
+}
 
