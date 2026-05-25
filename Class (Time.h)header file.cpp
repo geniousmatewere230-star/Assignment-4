b7351172670d81d229bae5334bfd0243534f505f
Genious Matewere <geniousmatewere230@gmail.com>
2026-05-25 14:39:40 +0200

    Add Time class definition in Time.h

---
+++ Class (Time.h)header file.cpp
@@ -0,0 +1,16 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include <iostream>
+
+class Time{
+private:    
+    [span_21](start_span)int hour;[span_21](end_span)
+    [span_22](start_span)int minute;[span_22](end_span)
+
+public:    
+    [span_23](start_span)void setTime(int h, int m);[span_23](end_span)
+    [span_24](start_span)void printTime();[span_24](end_span)
+};
+
+#endif
