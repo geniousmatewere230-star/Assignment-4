5d23d1862dc02bbf42e412b65b8a4fbe11817c70
Genious Matewere <geniousmatewere230@gmail.com>
2026-05-25 14:42:27 +0200

    Implement main function to set and print time

---
+++ Class main.cpp
@@ -0,0 +1,8 @@
+#include "Time.h"
+
+int main(){    
+    [span_28](start_span)Time t;[span_28](end_span)
+    [span_29](start_span)t.setTime(10,45);[span_29](end_span)
+    [span_30](start_span)t.printTime();[span_30](end_span)
+    [span_31](start_span)return 0;[span_31](end_span)
+}
