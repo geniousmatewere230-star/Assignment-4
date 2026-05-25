d09d9f42e5c33eeb9cae58ab82d27029f1f66071
Genious Matewere <geniousmatewere230@gmail.com>
2026-05-25 14:41:34 +0200

    Implement Time class with setTime and printTime methods

---
+++ Class(Time.cpp)implementation file.cpp
@@ -0,0 +1,11 @@
+#include "Time.h"
+using namespace std;
+
+void Time::setTime(int h, int m){        
+    [span_25](start_span)hour = h;[span_25](end_span)
+    [span_26](start_span)minute = m;[span_26](end_span)
+}    
+
+void Time::printTime(){        
+    [span_27](start_span)cout << hour << ":" << minute << endl;[span_27](end_span)
+} 
